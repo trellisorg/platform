a80ffa64169dc97308f6e15052bf9d098a53d3b2
jaybell <jaycooperbell@gmail.com>
2020-09-17 13:58:23 -0700

    build: update to nx 10

--- packages/nx-realm/jest.config.js
+++ packages/nx-realm/jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  name: 'nx-realm',
-  preset: '../../jest.config.js',
-  transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/packages/nx-realm',
+    name: 'nx-realm',
+    preset: '../../jest.config.js',
+    transform: {
+        '^.+\\.[tj]sx?$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+    coverageDirectory: '../../coverage/packages/nx-realm',
+    globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
 };
