6f9a059e75d82058be296345d835cd0ad7f3ec6c
jaybell <jaycooperbell@gmail.com>
2021-03-01 07:57:50 -0800

    fix(ngrx-universal-rehydrate): optimize meta reducer function to not check for platform every time

--- packages/ngrx-universal-rehydrate/src/lib/ngrx-universal-rehydrate.module.ts
+++ packages/ngrx-universal-rehydrate/src/lib/ngrx-universal-rehydrate.module.ts
@@ -14,42 +14,57 @@ export function rehydrateMetaReducer(
     config: NgrxUniversalHydrateConfig,
     ngrxUniversalHydrationService: NgrxUniversalHydrationService
 ): MetaReducer<unknown> {
-    return (reducer) => (state: any, action) => {
-        const isBrowser = isPlatformBrowser(platformId);
+    const isBrowser = isPlatformBrowser(platformId);
 
-        if (isBrowser && action.type === INIT) {
-            /**
-             * Rehydrate default state based on transferred data from the server
-             */
-            return reducer(
-                mergeStates(
-                    state,
-                    ngrxUniversalHydrationService.readState(),
-                    config.mergeStrategy
-                ),
-                action
-            );
-        } else if (!isBrowser) {
-            const newState = reducer(state, action);
-
-            if (newState) {
+    /**
+     * If we are in the browser return a meta reducer that will parse the initial state from
+     * the transfer state on INIT
+     */
+    if (isBrowser) {
+        return (reducer) => (state, action) => {
+            if (action.type === INIT) {
                 /**
-                 * Only persist state for store slices that are configured
+                 * Rehydrate default state based on transferred data from the server
                  */
-                if (config.stores?.length > 0) {
-                    ngrxUniversalHydrationService.state = config.stores.reduce(
-                        (prev, cur) => ({
-                            ...prev,
-                            [cur]: newState[cur],
-                        }),
-                        {}
-                    );
-                } else {
-                    ngrxUniversalHydrationService.state = newState;
-                }
-
-                return newState;
+                return reducer(
+                    mergeStates(
+                        state,
+                        ngrxUniversalHydrationService.readState(),
+                        config.mergeStrategy
+                    ),
+                    action
+                );
             }
+
+            return reducer(state, action);
+        };
+    }
+
+    /**
+     * If we are in the server environment return the meta reducer that will save the updated states into the transfer
+     * state based on the store slices configured.
+     */
+    return (reducer) => (state, action) => {
+        const newState = reducer(state, action);
+
+        if (newState) {
+            /**
+             * Only persist state for store slices that are configured
+             * TODO: Optimize this
+             */
+            if (config.stores?.length > 0) {
+                ngrxUniversalHydrationService.state = config.stores.reduce(
+                    (prev, cur) => ({
+                        ...prev,
+                        [cur]: newState[cur],
+                    }),
+                    {}
+                );
+            } else {
+                ngrxUniversalHydrationService.state = newState;
+            }
+
+            return newState;
         }
 
         return reducer(state, action);
