8869a527c50d787b854a1b1fc0f8c9836fc30dbc
jaybell <jaycooperbell@gmail.com>
2021-09-09 16:45:17 -0700

    build: fix secondary entry points once and for all

--- packages/rx-dynamic-component/jest.config.js
+++ packages/rx-dynamic-component/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     displayName: 'rx-dynamic-component',
     preset: '../../jest.preset.js',
-    setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+    setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
     globals: {
         'ts-jest': {
             stringifyContentPathRegex: '\\.(html|svg)$',
