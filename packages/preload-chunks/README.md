65c2d2a5409109eef09086e802acc02f64401aa5
jaybell <jaycooperbell@gmail.com>
2022-09-17 22:22:33 -0700

    chore(preload-chunks): publish 1.0.0

--- packages/preload-chunks/README.md
+++ packages/preload-chunks/README.md
@@ -1,11 +1,44 @@
-# preload-chunks
+# @trellisorg/preload-chunks
 
-This library was generated with [Nx](https://nx.dev).
+## Installation
 
-## Building
+`yarn add @trellisorg/preload-chunks`
 
-Run `nx build preload-chunks` to build the library.
+## Why
 
-## Running unit tests
+When rendering applications in Universal some lazy chunks will be loaded while the page is being rendered, usually these
+are the lazy loaded routes in your application but can be any lazy chunk. Once the static page is served to the browser
+needs to pull these assets again, to help speed this process up and prepare these assets while the browser
+is rendering the static page this plugin will add preload link tags so that lazy chunks can be scheduled ahead of time.
 
-Run `nx test preload-chunks` to execute the unit tests via [Jest](https://jestjs.io).
+## Prerequisites
+
+Have Angular Universal integrated into your application for server side rendering.
+
+## Usage
+
+In your `server.ts` file or wherever your rendering engine is initialized like so:
+
+```typescript
+server.engine(
+    'html',
+    ngExpressEngine({
+        bootstrap: AppServerModule,
+    })
+);
+```
+
+_Note: If you are using something other than `@nguniversal/express` this may look different._
+
+Provide the `provideChunkPreloader` function in the providers of your rendering engine, ensure that your path in the
+correct path to where your browser assets are stored on the local file system.
+
+```typescript
+server.engine(
+    'html',
+    ngExpressEngine({
+        bootstrap: AppServerModule,
+        providers: [provideChunkPreloader(join(__dirname, '../browser'))],
+    })
+);
+```
--- packages/preload-chunks/src/lib/patch-webpack.ts
+++ packages/preload-chunks/src/lib/patch-webpack.ts
@@ -3,10 +3,27 @@ import { ENVIRONMENT_INITIALIZER, inject } from '@angular/core';
 import { readdirSync } from 'fs';
 import { join } from 'path';
 
+/**
+ * Matches chunks created by the Angular CLI.
+ */
 const CHUNK_REGEX = /^[0-9]*.[a-z0-9]*.js$/;
 
+/**
+ * Global webpack variable that is available during SSR when Angular is being rendered by Universal. We know this exists
+ * so we define it (although it's very hacky)
+ */
 declare const __webpack_require__: { e: (moduleId: string) => unknown };
 
+/**
+ * Typically when building the server bundles hashes are not included in the file names like they are for the browser
+ * files because there is no caches that need to be broken. Because of this there is no direct 1:1 between file names
+ * of server files and browser files, but the chunk number or name will be the same so we store a map of moduleIds to
+ * look up the corresponding browser file by moduleId
+ *
+ * Browser: main.<hash>.js
+ * Server: main.js
+ * @param pathToBrowserFiles
+ */
 function readChunkMap(pathToBrowserFiles: string): Record<string, string> {
     const chunks = readdirSync(join(pathToBrowserFiles)).filter((fileName: string) =>
         new RegExp(CHUNK_REGEX).test(fileName)
@@ -21,6 +38,13 @@ function readChunkMap(pathToBrowserFiles: string): Record<string, string> {
     );
 }
 
+/**
+ * A provider function to be used within the server so that any lazy chunk that is loaded and thus needed to render
+ * the page that is sent to the client will have a `<link rel="modulepreload" href="<chunk path>">` tag in the SSR'd
+ * documents `<head>`
+ *
+ * @param pathToBrowserFiles
+ */
 export function provideChunkPreloader(pathToBrowserFiles: string) {
     const moduleMap = readChunkMap(pathToBrowserFiles);
     return {
