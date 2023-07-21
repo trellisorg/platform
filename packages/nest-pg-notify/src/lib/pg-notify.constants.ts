05ad1b5c68fcdeb840e7ce289f2634b0404424ef
jaybell <jaycooperbell@gmail.com>
2023-07-21 10:20:15 -0700

    chore(nest-pg-notify): make injection tokens use Symbol instead of strings

--- packages/nest-pg-notify/src/lib/pg-notify.constants.ts
+++ packages/nest-pg-notify/src/lib/pg-notify.constants.ts
@@ -1,11 +1,10 @@
-export const PG_NOTIFY_CHANNEL = 'pg-notify:channel' as const;
-export const PG_NOTIFY_NOTIFICATION = 'pg-notify:notification' as const;
+export const PG_NOTIFY_CHANNEL = Symbol('pg-notify:channel');
+export const PG_NOTIFY_NOTIFICATION = Symbol('pg-notify:notification');
 
-export const PG_NOTIFY_CONFIG = 'pg-notify:config' as const;
-export const PG_NOTIFY_ASYNC_CONFIG = 'pg-notify:async-config' as const;
-export const PG_NOTIFY_SUBSCRIBER = 'pg-notify:subscriber' as const;
+export const PG_NOTIFY_CONFIG = Symbol('pg-notify:config');
+export const PG_NOTIFY_SUBSCRIBER = Symbol('pg-notify:subscriber');
 
-export const PG_NOTIFY_REDIS = 'pg-notify:redis' as const;
-export const PG_NOTIFY_REDLOCK = 'pg-notify:redlock' as const;
+export const PG_NOTIFY_REDIS = Symbol('pg-notify:redis');
+export const PG_NOTIFY_REDLOCK = Symbol('pg-notify:redlock');
 
 export const PROCESSED_NOTIFICATION_PREFIX = 'pg-notify:notification' as const;
